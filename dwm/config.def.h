7d9853229af7ac88448e590eaf3182f7c1080635
iraulvelazquez <carlos.raul.velazquez.2004@gmail.com>
2026-01-13 19:14:30 -0600

    *

---
+++ dwm/config.def.h
@@ -0,0 +1,167 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <X11/XF86keysym.h>
+
+/* appearance */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 8;
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int broderpx  = 1;        /* border pixel of windows */ 
+static const int vertpad            = 8;       /* vertical padding of bar */
+static const int sidepad            = 8;       /* horizontal padding of bar */
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_teal[]        = "#436D63";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_teal,  col_teal  },
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance            title       tags mask     iscentered     isfloating   monitor */
+  { "kitty",    "kitty-center",     NULL,       0,            1,             1,           -1 },
+	{ "Gimp",     NULL,               NULL,       0,            0,             1,           -1 },
+	{ "Firefox",  NULL,               NULL,       1 << 8,       0,             0,           -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "j4-dmenu-desktop", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcenter[]  = { "kitty", "--name", "kitty-center", NULL };
+
+/* Comandos personalizados */
+static const char *flamegui[]  = { "flameshot", "gui", NULL };
+static const char *flamefull[] = { "flameshot", "full", "-c", NULL };
+static const char *flamescreen[] = { "flameshot", "screen", "-c", NULL };
+static const char *briup[]   = { "brightnessctl", "set", "+10%", NULL };
+static const char *bridown[] = { "brightnessctl", "set", "10%-", NULL };
+static const char *kbdup[]   = { "brightnessctl", "-d", "*backlight", "set", "+1", NULL };
+static const char *kbddown[] = { "brightnessctl", "-d", "*backlight", "set", "1-", NULL };
+static const char *kbtoggle[] = { "/bin/sh", "-c", "if setxkbmap -query | grep -q 'layout:.*latam'; then setxkbmap us && notify-send 'Teclado' 'US'; else setxkbmap latam && notify-send 'Teclado' 'LATAM'; fi && pkill -RTMIN+1 dwmblocks", NULL };
+/* Volumen */
+static const char *volup[]   = { "pamixer", "--increase", "5", NULL };
+static const char *voldown[] = { "pamixer", "--decrease", "5", NULL };
+static const char *volmute[] = { "pamixer", "--toggle-mute", NULL };
+static const char *micmute[] = { "pamixer", "--default-source", "--toggle-mute", NULL };
+
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+  { MODKEY,                       XK_s,      spawn,          {.v = termcenter } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	
+	/* Flameshot */
+	{ 0,                            XK_Print,  spawn,          {.v = flamegui } },
+	{ ShiftMask,                    XK_Print,  spawn,          {.v = flamefull } },
+	{ ControlMask,                  XK_Print,  spawn,          {.v = flamescreen } },
+	
+	/* Brillo */
+	{ 0,                XF86XK_MonBrightnessUp,   spawn,       {.v = briup } },
+	{ 0,                XF86XK_MonBrightnessDown, spawn,       {.v = bridown } },
+	
+	/* Brillo de teclado */
+	{ 0,                XF86XK_KbdBrightnessUp,   spawn,       {.v = kbdup } },
+	{ 0,                XF86XK_KbdBrightnessDown, spawn,       {.v = kbddown } },
+	
+	/* Toggle teclado US/LATAM */
+	{ MODKEY|ShiftMask,             XK_i,      spawn,          {.v = kbtoggle } },
+	
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  	/* Volumen */
+	{ 0,                XF86XK_AudioRaiseVolume,  spawn,       {.v = volup } },
+	{ 0,                XF86XK_AudioLowerVolume,  spawn,       {.v = voldown } },
+	{ 0,                XF86XK_AudioMute,         spawn,       {.v = volmute } },
+	{ 0,                XF86XK_AudioMicMute,      spawn,       {.v = micmute } },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
