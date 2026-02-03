bb7fe27f1b7900b222877f87e7ad9eca7ce892fa
iraulvelazquez <carlos.raul.velazquez.2004@gmail.com>
2026-02-03 11:31:43 -0600

    *

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"", "setxkbmap -query | awk '/layout/{print substr(toupper($2),1,2)}'",	5,		1},
-	{"WiFi: ", "iwgetid -r || echo 'Off'",					10,		0},
-	{"BAT: ", "cat /sys/class/power_supply/BAT0/capacity | awk '{print $1\"%\"}'",	60,		0},
-	{"", "date '+%b %d %H:%M'",					60,		0},
+  {"   ", "setxkbmap -query | awk '/layout/{print substr(toupper($2),1,2)}'",	5,		1},
+	{" 󰤨  ", "iwgetid -r || echo 'Off'",					10,		0},
+	{" 󰁹 ", "cat /sys/class/power_supply/BAT0/capacity | awk '{print $1\"%\"}'",	60,		0},
+  {" 󰖞  ", "curl -s 'wttr.in/?format=%t' | tr -d ' ' || echo '?'",	1800,		0},
+	{" 󰃰  ", "date '+%b %d %H:%M'",					60,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
